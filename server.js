7af2aa4c3545b0825378b92e4e90836cb560d818
Kyle Denis <kylenden@gmail.com>
2023-08-17 22:16:31 +1000

    Implemented Nodemon.
    Now  can be changed without rebuilding the entire project - simply refresh the page.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 	// Run the system `fortune` command and respond with the message
 	child_process.exec('fortune', (error, message) => {
 		if(error === null) {
-			res.send(message);
+			res.send("Your fortune is: " + message);
 		} else {
 			res.send('Error: ' + error);
 		}
